e6e45918c397faefecf1507f7c1deea0e11d2c86
Tolga Büyüktanır <tolgabuyuktanir@gmail.com>
2018-04-24 18:18:31 +0300

    Update resourceOptimizer.py

--- resourceOptimizer.py
+++ resourceOptimizer.py
@@ -5,20 +5,25 @@ import os
 def extractResourceUsage(out):
 	allContainer=out.split(" ");
 	allContainer = filter(None, allContainer)
-	if(len(allContainer)==14 and allContainer[12]>"50.00%"):
-		memoryOptimizer(allContainer[0],allContainer[5])
+	if(len(allContainer)==14 and (allContainer[12]>"90.00%" or allContainer[12]<"10.00%")):
+		memoryOptimizer(allContainer[0],allContainer[5],allContainer[12])
 	return
 
-def memoryOptimizer(id,totalMemory):
-	totalMemory=totalMemory[:-3]
+def memoryOptimizer(id,totalMemory,memPerc):
 	memoryUnit=totalMemory[-3:]
+	tMem=totalMemory[:-3]
+	factor=1
 	if(memoryUnit=="GiB"):
 		memoryUnit="g"
-	else:
+	if(memoryUnit=="MiB"):
 		memoryUnit="mb"
-	updateCommand="docker update "+id+" -m=\""+str(int(totalMemory)*2)+memoryUnit+"\"";
-	print(updateCommand)
-	os.system(updateCommand)
+	if(memPerc>"90.00%"):
+		factor=2
+	if(memPerc<"10.00%"):
+		factor=0.5
+	if(tMem.isdigit()):
+		updateCommand="docker update "+id+" -m=\""+str(float(tMem)*factor)+memoryUnit+"\""
+		os.system(updateCommand)
 	return
 
 def run(command):
@@ -31,9 +36,9 @@ def run(command):
 
 
 if __name__ == "__main__":
-    for path in run("docker stats --all --format \"table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}\""):
-        print path
+    for path in run("docker stats --format \"table {{.Container}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.NetIO}}\t{{.BlockIO}}\t{{.MemPerc}}\t{{.PIDs}}\""):
         extractResourceUsage(path)
+        #print path
 
 
 
